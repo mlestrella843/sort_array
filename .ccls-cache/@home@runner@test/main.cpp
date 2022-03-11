fd078480e9aa03d48fd3964d91670cb8109505d3
Maria <98559654+mlestrella843@users.noreply.github.com>
2022-03-11 18:51:55 +0000

    Anadi una funcion para que sordenara los arreglos.

--- .ccls-cache/@home@runner@test/main.cpp
+++ .ccls-cache/@home@runner@test/main.cpp
@@ -2,24 +2,40 @@
 using namespace std;
 
 //programs that sort an integer array.
-int main() {
-  
-//int a[4] = {10,25,35,40,55};
-int b[6] = {15,30,5,20,45,65}; 
 
-int i; int j; int sizeb = 6; int temp = 0;
-  
+//function that sort an array and prints.
+void sort_array( int array[], int sizeb ){
+  int i;int j; int temp=0;
   for (j=0; j<sizeb; j++){  
      for (i=0; i<sizeb ; i++){    
-        if( b[i] > b[i+1] ){
-            temp = b[i];
-            b[i] = b[i+1];
-            b[i+1] = temp;    
+        if( array[i] > array[i+1] ){
+            temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;    
         }       
      }   
-  }
-  for(int i=0; i< sizeb; i++){
-    cout<<b[i]; cout<<",";
-  }  
+  } 
+    
+}
+
+
+//final functio
+
+int main() {
+  
+//int a[5] = {10,25,35,40,55};
+int b[6] = {15,30,5,20,45,65}; 
+//int c[11]={};  
+int sizeb = 6; 
+//int sizec = 11;
+   
+sort_array(b, 6);
+cout<<"Este arreglo b ordenado\n";  
+for(int i=0; i< sizeb; i++){    
+cout<<b[i]; cout<<",";
+  }    
+
   return 0;
+
+  
 }
